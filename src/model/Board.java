4592ad1d57f950a640caf458cf83ab3bbf52e533
abrahambijin <abrahambijin@gmail.com>
2016-04-12 03:34:25 +1000

    Board class

--- src/model/Board.java
+++ src/model/Board.java
@@ -3,23 +3,26 @@ package model;
 /**
  * Created by mitulmanish on 26/03/2016.
  */
-public class Board {
-
-    private final int BOARDSIZE = 10;
+public class Board
+{
 
     private GameItem[][] warZone;
 
-    public Board() {
-        this.warZone = new GameItem[BOARDSIZE][BOARDSIZE];
+    public Board(int boardSize)
+    {
+
+        this.warZone = new GameItem[boardSize][boardSize];
     }
 
-    public void placeGameItem(GameItem item){
+    public void placeGameItem(GameItem item)
+    {
 
         Point position = item.getPosition();
         int xCoordinate = position.getXCoordinate();
         int yCoordinate = position.getYCoordinate();
         // the cell must be empty to place a new game item
-        if(warZone[xCoordinate][yCoordinate] == null){
+        if (warZone[xCoordinate][yCoordinate] == null)
+        {
             warZone[xCoordinate][yCoordinate] = item;
         }
     }
