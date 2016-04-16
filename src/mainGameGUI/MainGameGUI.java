2f0d4da429de1cee28ba639f03328f06f8946838
abrahambijin <abrahambijin@gmail.com>
2016-04-17 01:17:59 +1000

    Revert "new"
    This reverts commit 4ef2e8fa49bacf39b6326631f9717a831e47f1c3.

--- src/mainGameGUI/MainGameGUI.java
+++ src/mainGameGUI/MainGameGUI.java
@@ -1,6 +1,5 @@
 package mainGameGUI;
 
-import model.Game;
 import view.GameGUI;
 /**
  * Created by ankurdabral on 15/04/2016.
@@ -8,7 +7,6 @@ import view.GameGUI;
 public class MainGameGUI {
 
     public static void main (String[] arg){
-
 
         GameGUI gui = new GameGUI();
         //gui.setVisible(true);
